67c549f19d81fdb5f2c838dcca083932670101ef
Jeon, Yeo Hun <21500630@handong.edu>
2020-03-08 16:45:03 +0900

    add main class to test each method

---
+++ src/main/java/parser/isel/csee/edu/handong/test.java
@@ -0,0 +1,18 @@
+package parser.isel.csee.edu.handong;
+
+import java.util.List;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ASTParserImpl ast = new ASTParserImpl();
+		List<String> c = ast.fileReader("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-280/src/java/org/apache/commons/math/ArgumentOutsideDomainException.java");
+		
+		for (String str : c) {
+			System.out.println(str);
+		}
+
+	}
+
+}
