b71293f3a18b56d1f5ada5dc2dac0fc95c132c35
Jeon, Yeo Hun <21500630@handong.edu>
2020-03-08 16:44:23 +0900

    set interface of MethodParser

---
+++ src/main/java/parser/isel/csee/edu/handong/MethodParser.java
@@ -0,0 +1,9 @@
+package parser.isel.csee.edu.handong;
+
+import java.util.ArrayList;
+
+public interface MethodParser {
+	ArrayList<Integer> run();
+	ArrayList<String> statementSearcher();
+	ArrayList<Integer> makeList(); 
+}
