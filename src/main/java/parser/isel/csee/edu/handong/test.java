23bb58d473d343006cd00a57a3650d9c69cc1c0a
Jeon, Yeo Hun <21500630@handong.edu>
2020-03-15 15:54:41 +0900

    update test main class to check using utils package and getStatement method in JavaASTParser

--- src/main/java/parser/isel/csee/edu/handong/test.java
+++ src/main/java/parser/isel/csee/edu/handong/test.java
@@ -1,18 +1,30 @@
 package parser.isel.csee.edu.handong;
 
+import java.io.File;
 import java.util.List;
+
+import util.isel.csee.edu.handong.*;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JavaASTParserImpl ast = new JavaASTParserImpl();
-		List<String> c = ast.fileReader("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-280/src/java/org/apache/commons/math/ArgumentOutsideDomainException.java");
+		JavaCode2String J2S = new JavaCode2String();
+		File file = new File("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-340/src/main/java/org/apache/commons/math/fraction/BigFraction.java");
+		String source = J2S.FiletoString(file);
+		CodePreprocessor cp = new CodePreprocessor(source);
+		source = cp.run();
+	
+		JavaASTParserImpl ast = new JavaASTParserImpl(source);
+		List<String> c = ast.fileReader("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-340/src/main/java/org/apache/commons/math/fraction/BigFraction.java");
 		
-		for (String str : c) {
+		
+		System.out.println("====================================");
+		/*for (String str : c) {
 			System.out.println(str);
-		}
+		}*/
 
+		ast.getStatements();
 	}
 
 }
