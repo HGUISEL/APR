40576fe017a1cb5a87174e9c826a5ccb1853138c
Jeon, Yeo Hun <21500630@handong.edu>
2020-04-12 17:11:00 +0900

    Modified testing main class. We need to test getStatement() method in JavaASTParser class to see the method bring all the needed statements in order successfully

--- src/main/java/parser/isel/csee/edu/handong/test.java
+++ src/main/java/parser/isel/csee/edu/handong/test.java
@@ -1,30 +1,19 @@
 package parser.isel.csee.edu.handong;
 
 import java.io.File;
-import java.util.List;
 
 import util.isel.csee.edu.handong.*;
 
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		JavaCode2String J2S = new JavaCode2String();
 		File file = new File("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-340/src/main/java/org/apache/commons/math/fraction/BigFraction.java");
 		String source = J2S.FiletoString(file);
-		CodePreprocessor cp = new CodePreprocessor(source);
-		source = cp.run();
 	
-		JavaASTParserImpl ast = new JavaASTParserImpl(source);
-		List<String> c = ast.fileReader("/Users/yeohunjeon/Desktop/ISEL/APR/dataset/Math-issue-340/src/main/java/org/apache/commons/math/fraction/BigFraction.java");
+		JavaASTParser ast = new JavaASTParser(source);
 		
-		
-		System.out.println("====================================");
-		/*for (String str : c) {
-			System.out.println(str);
-		}*/
-
-		ast.getStatements();
+		ast.getFullClassName();
 	}
 
 }
