a2d475704c21392e98ce272270904002f3010098
Jeon, Yeo Hun <21500630@handong.edu>
2020-04-12 17:06:27 +0900

    set basic methods frames to implement MethodParser class

--- src/main/java/parser/isel/csee/edu/handong/MethodParser.java
+++ src/main/java/parser/isel/csee/edu/handong/MethodParser.java
@@ -2,8 +2,18 @@ package parser.isel.csee.edu.handong;
 
 import java.util.ArrayList;
 
-public interface MethodParser {
-	ArrayList<Integer> run();
-	ArrayList<String> statementSearcher();
-	ArrayList<Integer> makeList(); 
+public class MethodParser {
+	ArrayList<Integer> run(){
+		return null;
+		
+	}
+	ArrayList<String> statementSearcher(){
+		return null;
+		
+	}
+	ArrayList<Integer> makeList(){
+		return null;
+		
+	}
+
 }
