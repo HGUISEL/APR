e550a14e9b146358956196e38303d70a802f12c0
S0rrow <jae9hell@gmail.com>
2022-02-04 17:29:42 +0900

    - divided confixes_batch.py into 3 python files - fix-target-env_confix-launcher.py executes confix overall - fix-target-env_new-old-file-generator.py clones Defects4j bugs' new and old repository along the bug overall - fix-target-integrated.py executes other 2 python files simultaneously

--- fix-target-env_confix-launcher.py
+++ fix-target-env_confix-launcher.py
@@ -4,6 +4,7 @@ import os
 import datetime as dt
 import pandas as pd
 import csv
+import time
 
 from colorama import Fore, Back, Style # Colorized output
 
@@ -21,18 +22,6 @@ def print_complete(msg):
 def print_error(msg):
     print(Back.RED + Fore.WHITE + msg + Style.RESET_ALL)
 
-def print_help(cmd):
-    options_list = ['repository', 'commit_id', 'faulty_file', 'faulty_line', 'source_path', 'target_path', 'test_list', 'test_target_path', 'compile_target_path', 'build_tool']
-
-    print("Usage :")
-    print(f"- Defects4J check         : {cmd} --defects4j <Identifier>-<Bug-ID>")
-    print(f"- Custom repository check : {cmd} --repository <github_repository> --commit_id <commit_id> --faulty_file <path_from_project_root> --faulty_line <buggy_line_number> --source_path <source_path> --target_path <target_path> --test_list <test_class_name> --test_target_path <test_target_path> --compile_target_path <compile_target_path> --build_tool <maven_or_gradle>")
-    print(f"- Custom repository check : {cmd} --case_file <file_containing_arguments>")
-    print()
-    print("When bringing arguments from file, it should contain arguments in the following order:")
-    for option in options_list:
-        print(f"- {option}")
-    print()
 
 def run_command(cmd):
     print_info(f'||| > Executing command "{cmd}"')
@@ -50,76 +39,64 @@ def run_command(cmd):
 class AbnormalExitException(Exception):
     pass
 
-def prepare(root, hash_id, identifier, bug_id):
-    try:
-        target_dir = f"{root}/target/{hash_id}"
-
-        executing_command = f"python3 ./pool/runner_web/commit_collector_web.py -d true -h {hash_id} -i {identifier}-{bug_id}"
-        print_status("||| Prep.Step 1. Launching Commit Collector...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
-
-
-        print_status("||| Prep.Step 2. Manually preparing pool source for ConFix...")
-        print("\n" + "=" * 80 + "\n")
-
-        try:
-            bfic = pd.read_csv(f'{target_dir}/commit_collector/BFIC.csv', names = ['Project', 'D4J ID', 'Faulty file path', 'Faulty line', 'FIC_sha', 'BFIC_sha']).values[1]
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}b -w buggy") == 0, "checkout for buggy project failed"
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}f -w fixed") == 0, "checkout for fixed project failed"
-
-            assert os.system(f"cd {target_dir}; mkdir -p outputs; mkdir -p outputs/prepare_pool_source") == 0, "what?"
-            assert os.system(f"cp {target_dir}/buggy/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_old.java") == 0, "copying buggy file failed"
-            assert os.system(f"cp {target_dir}/fixed/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_new.java") == 0, "copying fixed file failed"
-        except AssertionError as e:
-            print(e)
-            raise AbnormalExitException
-
-        print("\n" + "=" * 80 + "\n")
-
-        executing_command = f"python3 ./confix/run_confix_web.py -d true -h {hash_id}"
-        print_status("||| Step 3. Executing ConFix...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
-
-    except AbnormalExitException as e:
-        pass
-
 def launch(root, hash_id, identifier, bug_id):
     try:
-        target_dir = f"{root}/target/{hash_id}"
-
-        executing_command = f"python3 ./pool/runner_web/commit_collector_web.py -d true -h {hash_id} -i {identifier}-{bug_id}"
-        print_status("||| Step 1. Launching Commit Collector...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
-
-
-        print_status("||| Step 2. Manually preparing pool source for ConFix...")
-        print("\n" + "=" * 80 + "\n")
-
-        try:
-            bfic = pd.read_csv(f'{target_dir}/commit_collector/BFIC.csv', names = ['Project', 'D4J ID', 'Faulty file path', 'Faulty line', 'FIC_sha', 'BFIC_sha']).values[1]
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}b -w buggy") == 0, "checkout for buggy project failed"
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}f -w fixed") == 0, "checkout for fixed project failed"
-
-            assert os.system(f"cd {target_dir}; mkdir -p outputs; mkdir -p outputs/prepare_pool_source") == 0, "what?"
-            assert os.system(f"cp {target_dir}/buggy/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_old.java") == 0, "copying buggy file failed"
-            assert os.system(f"cp {target_dir}/fixed/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_new.java") == 0, "copying fixed file failed"
-        except AssertionError as e:
-            print(e)
-            raise AbnormalExitException
-
-        print("\n" + "=" * 80 + "\n")
-
-        executing_command = f"python3 ./confix/run_confix_web.py -d true -h {hash_id}"
         print_status("||| Step 3. Executing ConFix...")
+
+        target_dir = f"{root}/target/{hash_id}_{identifier}-{bug_id}"
+        output_dir = f"{target_dir}/outputs"
+
+        just_target = target_dir
+
+
+        # pwd is APR_Contents/APR/
+        root = os.getcwd()
+        # currently, we are running confix in APR directory
+
+
+        perfect_info = pd.read_csv(f"{output_dir}/commit_collector/BFIC.csv", names=['Project','D4J ID','Faulty file path','faulty line','FIC sha','BFIC sha']).values[1]
+        target_project, target_id, perfect_faulty_path, perfect_faulty_line = perfect_info[0], perfect_info[1], perfect_info[2], perfect_info[3]
+
+        perfect_faulty_class, foo = perfect_faulty_path.split(".")
+        perfect_faulty_class = perfect_faulty_class.replace("/", ".")
+
+        target_dir = target_dir +"/"+ target_project
+
+        ## prepare setup before running confix
+
+        ### for D4J projects
+        os.system(f"rm -rf {target_dir}; defects4j checkout -p {target_project} -v {target_id}b -w {target_dir}")
+        os.system(f"cp {root}/confix/coverages{target_project.lower()}/{target_project.lower()}{target_id}b/coverage-info.obj {target_dir}")
+        os.system(f"cd {target_dir}; {root}/confix/scripts/config.sh {target_project} {target_id} {perfect_faulty_class} {perfect_faulty_line}")
+        os.system(f"cd {target_dir}; echo \"pool.source={just_target}/outputs/prepare_pool_source\" >> confix.properties; ")
+
+
+        print("Configuration finished.")
+
+        print("Executing ConFix...")
+        os.system(f"cd {target_dir}; /usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xmx4g -cp ../../../confix/lib/las.jar:../../../confix/lib/confix-ami_torun.jar -Duser.language=en -Duser.timezone=America/Los_Angeles com.github.thwak.confix.main.ConFix > log.txt")
+        print("ConFix Execution Finished.")
+
+
+        os.system("echo \"end\" >> "+ just_target+"/status.txt")
+
+
+        if not os.path.isfile(f"{target_dir}/patches/0/{perfect_faulty_path}"):
+            print("ConFix failed to generate plausible patch.")
+            sys.exit(-63)
+
+        else:
+            git_stream = os.popen(f"cd ~; git diff {target_dir}/{perfect_faulty_path} {target_dir}/patches/0/{perfect_faulty_path}")
+
+            foo = str(git_stream.read()).split('\n')
+
+            os.system(f"echo \"diff --git a a\" > {just_target}/diff_file.txt")
+            os.system(f"echo \"--- {perfect_faulty_path}\" >> {just_target}/diff_file.txt")
+            os.system(f"echo \"+++ {perfect_faulty_path}\" >> {just_target}/diff_file.txt")
+            
+            for i in range(4, len(foo)):
+                os.system(f"echo \"{foo[i]}\" >> {just_target}/diff_file.txt")
+        executing_command = f"python3 ./confix/run_confix_web.py -d true -h {hash_id}"
         
         exit_code = run_command(executing_command)
         if exit_code != 0:
@@ -128,38 +105,85 @@ def launch(root, hash_id, identifier, bug_id):
     except AbnormalExitException as e:
         pass
 
+
+# def main(argv):
+#     # Run Three steps: Commit Collector, Prepare Pool Source, and ConFix.
+
+
+#     identifiers = ("Closure", "Lang", "Math", "Time")
+#     timestamp = dt.datetime.now().strftime('%Y%m%d%H%M%S')
+
+#     hash_id = f'Batch_{timestamp}_{identifiers}'
+
+#     # extract bug ids from here
+#     # bug_id = None
+
+#     # do a loop here:
+
+#     whole_end = dt.datetime.now()
+#     elapsed_time = (whole_end - whole_start)
+
+#     print()
+#     if exit_code != 0:
+#         print_error("||| SimonFix failed to find out the patch, or aborted due to abnormal exit.")
+#         print_info(f"||| Elapsed Time : {elapsed_time}")
+#     else:
+#         print_complete("||| SimonFix succeeded to find out the plausible patch.")
+#         print_info(f"||| Elapsed Time : {elapsed_time}")
+#         print_info("||| Generated Patch Content:")
+#         print()
+#         exit_code = os.system(f"cat ./target/{hash_id}/diff_file.txt")
+#     print()
+#     print("=" * 80)
+
+
+def main(args):
+    os.system("cd ./confix/ConFix-code; mvn clean package; cp target/confix-0.0.1-SNAPSHOT-jar-with-dependencies.jar /home/codemodel/hans/APR/confix/lib/confix-ami_torun.jar")
+
+    bugs = []
+
+    try:
+        with open(args[2], 'r') as infile:
+            bugs = infile.read().splitlines()
+    except Exception as e: # Assume that the script failed to open the file.
+        print(e)
+        sys.exit(-1)
+
+    hash_id = f'batch_{args[3]}'
+
+    for bug in bugs:
+        identifier, bug_id = bug.split('_')
+
+        while True:
+            if os.path.isdir(f"{args[1]}/target/batch_{args[2]}_{identifier}-{bug_id}"):
+                launch(args[1], hash_id, identifier, bug_id)
+                break
+            else:
+                time.sleep(1)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
 def main(argv):
-    # Run Three steps: Commit Collector, Prepare Pool Source, and ConFix.
-
-
-    identifiers = ("Closure", "Lang", "Math", "Time")
-    timestamp = dt.datetime.now().strftime('%Y%m%d%H%M%S')
-
-    hash_id = f'Batch_{timestamp}_{identifiers}'
-
-    # extract bug ids from here
-    # bug_id = None
-
-    # do a loop here:
-
-    whole_end = dt.datetime.now()
-    elapsed_time = (whole_end - whole_start)
-
-    print()
-    if exit_code != 0:
-        print_error("||| SimonFix failed to find out the patch, or aborted due to abnormal exit.")
-        print_info(f"||| Elapsed Time : {elapsed_time}")
-    else:
-        print_complete("||| SimonFix succeeded to find out the plausible patch.")
-        print_info(f"||| Elapsed Time : {elapsed_time}")
-        print_info("||| Generated Patch Content:")
-        print()
-        exit_code = os.system(f"cat ./target/{hash_id}/diff_file.txt")
-    print()
-    print("=" * 80)
+    try:
+        opts, args = getopt.getopt(argv[1:], "d:i:h:", ["defects4J", "input", "hash"])
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    is_D4J = False
+    hash_input = ""
+    for o, a in opts:
+        if o in ("-d", "--defects4J"):
+            is_D4J = True
+        elif o in ("-i", "--input"):
+            input_string = a
+        elif o in ("-h", "--hash"):
+            hash_input = a
+        else:
+            assert False, "unhandled option"
 
     
 
 
-if __name__ == '__main__':
-    main(sys.argv)
+    root = os.getcwd()
--- fix-target-env_new-old-file-generator.py
+++ fix-target-env_new-old-file-generator.py
@@ -21,18 +21,6 @@ def print_complete(msg):
 def print_error(msg):
     print(Back.RED + Fore.WHITE + msg + Style.RESET_ALL)
 
-def print_help(cmd):
-    options_list = ['repository', 'commit_id', 'faulty_file', 'faulty_line', 'source_path', 'target_path', 'test_list', 'test_target_path', 'compile_target_path', 'build_tool']
-
-    print("Usage :")
-    print(f"- Defects4J check         : {cmd} --defects4j <Identifier>-<Bug-ID>")
-    print(f"- Custom repository check : {cmd} --repository <github_repository> --commit_id <commit_id> --faulty_file <path_from_project_root> --faulty_line <buggy_line_number> --source_path <source_path> --target_path <target_path> --test_list <test_class_name> --test_target_path <test_target_path> --compile_target_path <compile_target_path> --build_tool <maven_or_gradle>")
-    print(f"- Custom repository check : {cmd} --case_file <file_containing_arguments>")
-    print()
-    print("When bringing arguments from file, it should contain arguments in the following order:")
-    for option in options_list:
-        print(f"- {option}")
-    print()
 
 def run_command(cmd):
     print_info(f'||| > Executing command "{cmd}"')
@@ -52,7 +40,7 @@ class AbnormalExitException(Exception):
 
 def prepare(root, hash_id, identifier, bug_id):
     try:
-        target_dir = f"{root}/target/{hash_id}"
+        target_dir = f"{root}/target/{hash_id}_{identifier}-{bug_id}"
 
         executing_command = f"python3 ./pool/runner_web/commit_collector_web.py -d true -h {hash_id} -i {identifier}-{bug_id}"
         print_status("||| Prep.Step 1. Launching Commit Collector...")
@@ -79,87 +67,25 @@ def prepare(root, hash_id, identifier, bug_id):
 
         print("\n" + "=" * 80 + "\n")
 
-        executing_command = f"python3 ./confix/run_confix_web.py -d true -h {hash_id}"
-        print_status("||| Step 3. Executing ConFix...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
-
     except AbnormalExitException as e:
         pass
 
-def launch(root, hash_id, identifier, bug_id):
+
+def main(args):
+    bugs = []
+
     try:
-        target_dir = f"{root}/target/{hash_id}"
+        with open(args[2], 'r') as infile:
+            bugs = infile.read().splitlines()
+    except Exception as e: # Assume that the script failed to open the file.
+        print(e)
+        sys.exit(-1)
 
-        executing_command = f"python3 ./pool/runner_web/commit_collector_web.py -d true -h {hash_id} -i {identifier}-{bug_id}"
-        print_status("||| Step 1. Launching Commit Collector...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
+    hash_id = f'batch_{args[3]}'
 
-
-        print_status("||| Step 2. Manually preparing pool source for ConFix...")
-        print("\n" + "=" * 80 + "\n")
-
-        try:
-            bfic = pd.read_csv(f'{target_dir}/commit_collector/BFIC.csv', names = ['Project', 'D4J ID', 'Faulty file path', 'Faulty line', 'FIC_sha', 'BFIC_sha']).values[1]
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}b -w buggy") == 0, "checkout for buggy project failed"
-            assert os.system(f"cd {target_dir}; defects4j checkout -p {identifier} -v {bug_id}f -w fixed") == 0, "checkout for fixed project failed"
-
-            assert os.system(f"cd {target_dir}; mkdir -p outputs; mkdir -p outputs/prepare_pool_source") == 0, "what?"
-            assert os.system(f"cp {target_dir}/buggy/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_old.java") == 0, "copying buggy file failed"
-            assert os.system(f"cp {target_dir}/fixed/{bfic['Faulty file path']} {target_dir}/outputs/prepare_pool_source/{identifier}_rank-1_new.java") == 0, "copying fixed file failed"
-        except AssertionError as e:
-            print(e)
-            raise AbnormalExitException
-
-        print("\n" + "=" * 80 + "\n")
-
-        executing_command = f"python3 ./confix/run_confix_web.py -d true -h {hash_id}"
-        print_status("||| Step 3. Executing ConFix...")
-        
-        exit_code = run_command(executing_command)
-        if exit_code != 0:
-            raise AbnormalExitException
-
-    except AbnormalExitException as e:
-        pass
-
-def main(argv):
-    # Run Three steps: Commit Collector, Prepare Pool Source, and ConFix.
-
-
-    identifiers = ("Closure", "Lang", "Math", "Time")
-    timestamp = dt.datetime.now().strftime('%Y%m%d%H%M%S')
-
-    hash_id = f'Batch_{timestamp}_{identifiers}'
-
-    # extract bug ids from here
-    # bug_id = None
-
-    # do a loop here:
-
-    whole_end = dt.datetime.now()
-    elapsed_time = (whole_end - whole_start)
-
-    print()
-    if exit_code != 0:
-        print_error("||| SimonFix failed to find out the patch, or aborted due to abnormal exit.")
-        print_info(f"||| Elapsed Time : {elapsed_time}")
-    else:
-        print_complete("||| SimonFix succeeded to find out the plausible patch.")
-        print_info(f"||| Elapsed Time : {elapsed_time}")
-        print_info("||| Generated Patch Content:")
-        print()
-        exit_code = os.system(f"cat ./target/{hash_id}/diff_file.txt")
-    print()
-    print("=" * 80)
-
-    
-
+    for bug in bugs:
+        identifier, bug_id = bug.split('_')
+        prepare(args[1], hash_id, identifier, bug_id)
 
 if __name__ == '__main__':
     main(sys.argv)
